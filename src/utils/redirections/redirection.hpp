480563db95b4c2979c11ee4cf1765039c3f94605
Pritish Tomar <pritish.tomar.dev@gmail.com>
2026-06-23 16:59:36 +0530

    add header file for I/O redirection

---
+++ src/utils/redirections/redirection.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include<string>
+#include<vector>
+
+using namespace std;
+
+enum class RedirectionType{
+
+    STDOUT_OVERWRITE, // >,
+    STDOUT_APPEND, // >>,
+    STDIN_FILE, // <,
+    STDER_FILE, // 2>,
+    STDERR_TO_STDOUT, //2>&1,
+    BOTH_FILE, // &>,
+};
+
+struct Redirection{
+    RedirectionType type;
+    string file; // empty for STDERR_TO_STDOUT
+};
+
+void parseRedirections(string &raw, vector<Redirection> & out);
