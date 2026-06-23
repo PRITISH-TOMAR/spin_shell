12429cfd9643c293f68bdcc793ee3955973c6f07
Pritish Tomar <pritish.tomar.dev@gmail.com>
2026-06-23 14:21:14 +0530

    feat: implement grep command and wire all into dispatch

--- src/commands/dispatch.cpp
+++ src/commands/dispatch.cpp
@@ -10,6 +10,10 @@
 #include "src/commands/handlers/tools/rmdir.hpp"
 #include "src/commands/handlers/builtins/history.hpp"
 #include "src/commands/handlers/tools/clear.hpp"
+#include "src/commands/handlers/tools/grep.hpp"
+#include "src/commands/handlers/tools/cp.hpp"
+#include "src/commands/handlers/tools/mv.hpp"
+#include "src/commands/handlers/builtins/help.hpp"
 
 
 int dispatchCommand(Command cmd, const ParsedInput& parsed, ShellState& state)
@@ -23,17 +27,16 @@ int dispatchCommand(Command cmd, const ParsedInput& parsed, ShellState& state)
 
     case Command::LS: return handleLs(parsed, state);
 
-    // TODO: wire these as their handlers are implemented
-    // case Command::GREP:    return handleGrep(parsed, state);
+    case Command::GREP:    return handleGrep(parsed, state);
     case Command::MKDIR:   return handleMkdir(parsed, state);
     case Command::RM:      return handleRm(parsed, state);
     case Command::RMDIR:   return handleRmdir(parsed, state);
-    // case Command::CP:      return handleCp(parsed, state);
-    // case Command::MV:      return handleMv(parsed, state);
+    case Command::CP:      return handleCp(parsed, state);
+    case Command::MV:      return handleMv(parsed, state);
     case Command::TOUCH:   return handleTouch(parsed, state);
     case Command::CLEAR:   return handleClear(parsed, state);
     case Command::HISTORY: return handleHistory(parsed, state);
-    // case Command::HELP:    return handleHelp(parsed, state);
+    case Command::HELP:    return handleHelp(parsed, state);
 
     default: return 0;
     }
