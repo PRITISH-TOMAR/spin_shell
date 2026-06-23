5938fd5fb1846cb938735efefbc1ebbae97ff323
Pritish Tomar <pritish.tomar.dev@gmail.com>
2026-06-23 16:01:51 +0530

    feat: implement glob expansion for unquoted arguments

--- src/utils/parser/parser.hpp
+++ src/utils/parser/parser.hpp
@@ -8,9 +8,10 @@ using namespace std;
 struct ParsedInput {
     string                      command;
     vector<string>              rawArgs;
-    vector<string>              files;        // non-flag args
-    unordered_map<char, bool>   shortFlags;   // -n -b -a -l
-    unordered_map<string, bool> longFlags;    // --number --help
+    vector<bool>                rawArgsHasGlob; // parallel to rawArgs: true if token had unquoted * ? [
+    vector<string>              files;          // non-flag args
+    unordered_map<char, bool>   shortFlags;     // -n -b -a -l
+    unordered_map<string, bool> longFlags;      // --number --help
 };
 
 ParsedInput parseInput(const string& input);
